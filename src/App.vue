e6d18580ed917c4d05c32cd285848265b78e92f8
anthony74742 <antho230104@gmail.com>
2024-03-06 16:30:35 +0100

    import 3D model + animation

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { TresCanvas } from '@tresjs/core';
+import { OrbitControls } from '@tresjs/cientos';
+import Astronaut from './components/Astronaut.vue';
+</script>
+
+<template>
+  <TresCanvas clear-color="#82DBC5" window-size>
+    <TresPerspectiveCamera
+      :position="[3, 3, 3]"
+      :fov="100"
+      :look-at="[0, 0, 0]"
+    />
+    <OrbitControls />
+    <Suspense>
+      <Astronaut />
+    </Suspense>
+    <TresAmbientLight :intensity="2" />
+    <TresDirectionalLight cast-shadow :position="[0, 2, 0]" :intensity="1" />
+  </TresCanvas>
+</template>
+
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+}
+#app {
+  height: 100%;
+  width: 100%;
+}
+</style>
---
+++ src/components/Astronaut.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useAnimations, useGLTF } from '@tresjs/cientos'
+
+// const { scene: model, animations } = await useGLTF(
+//   '../assets/Car.glb',
+// )
+const { scene: model, animations } = await useGLTF(
+  '/assets/astronaut/scene.gltf',
+)
+
+const { actions } = useAnimations(animations, model)
+
+console.log(actions)
+
+const currentAction = ref(actions.wave)
+
+currentAction.value.play()
+
+</script>
+
+<template>
+  <primitive :object="model" />
+</template>
